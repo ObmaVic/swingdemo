7685ee93656bfec3a5c4abc69a4fe3daf48b0930
Martin <yumi61657110@126.com>
2020-08-30 19:09:59 +0800

    Applet示例

---
+++ src/appletdemo/applet/HelloApplet.java
@@ -0,0 +1,21 @@
+package appletdemo.applet;
+
+import javax.swing.*;
+
+/**
+ * @Description 通过继承JApplet类实现一个简单的applet程序
+ * @Author ***
+ * @Date 2020/8/30 17:10
+ */
+public class HelloApplet extends JApplet {
+    /**
+     * Applet中有4个主要的函数，分别为：init，start，stop和destroy，
+     * init相当于普通Java程序中的main函数
+     * 每一个applet程序都会从init函数开始运行
+     */
+    @Override
+    public void init(){
+        JLabel jLabel = new JLabel("Hello Applet！！！");
+        add(jLabel);
+    }
+}
